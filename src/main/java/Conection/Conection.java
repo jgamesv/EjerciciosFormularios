1f49604ad39eac316bf5c8e37c32cca6ae91c976
Jorge <jgamesv18@gmail.com>
2022-10-02 19:06:23 +0200

    second

---
+++ src/main/java/Conection/Conection.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conection;
+
+/**
+ *
+ * @author jgame
+ */
+public class Conection {
+   /* static public Connection getConection() throws SQLException{
+    Connection con;
+    try {
+        return con
+    }catch (SQLException e){
+    }
+    
+    }*/
+}
